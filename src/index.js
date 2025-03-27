cc2bf709bfd65ac324a399578d5dac92f39e3998
ruki007 <ru07ki02@outlook.jp>
2025-03-27 13:19:31 +0900

    環境構築

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import 'core-js/stable'; // Modern JavaScript features polyfill
-import 'regenerator-runtime/runtime'; // Async/await polyfill
+import 'babel-polyfill'; // 必要なポリフィルを提供
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
